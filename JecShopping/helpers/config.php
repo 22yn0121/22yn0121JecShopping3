a7dba0d83d7c88173d155165ccd85d90833644c7
unknown <22yn0121@JN01.JEC.AC.JP>
2023-08-01 20:26:05 +0900

    DB接続文字列を732実習室からAzureSQLデータベースへ変更

--- JecShopping/helpers/config.php
+++ JecShopping/helpers/config.php
@@ -1,5 +1,5 @@
 <?php
     // DB接続設定（MySQL用）
-    define('DSN', 'sqlsrv:server = tcp:22yn0121db.database.windows.net,1433; Database = ');
+    define('DSN', 'sqlsrv:server = tcp:22yn0121db.database.windows.net,1433; Database = 22yn0121');
     define('DB_USER', 'yndb');
     define('DB_PASSWORD', 'Pa$$word1234');
